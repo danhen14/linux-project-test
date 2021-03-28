31b3091c86b2e3799b42854b32a458b8e1226ba8
danhen14 <danhen14@yahoo.com>
2021-03-28 11:23:08 -0700

    Var

---
+++ function.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+divided(){
+echo "----------------------------"
+echo "        Division           "
+echo "---------------------------"
+echo "The division of $1 by $3 is $div
+printf "%0.2f\n" $T
+
+divide $sum1 $sum2
---
+++ var.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAME=Danny
+LNAME=Tadesse
+AGE=99
+
+echo "My name is $NAME $LNAME and I am $AGE years old"

fe54746d43d3d17c35da7c5c796f9f8c17162360
danhen14 <danhen14@yahoo.com>
2021-04-01 19:26:42 -0700

    initial master

---
+++ template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# -------------------------------------------------
+# Author: Danny Tu B Z
+# Title: Maths Equation and Syntax
+# Purpose: Learning Linux bash shell script and Maths!
+# Date: April fool 2021
+
+# Description:
+
+# Login Section
+
+# End of priject
